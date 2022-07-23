e74cab19cda87606d8e783e3f7ab6f5c26b78335
NaughtyEric <jia_206551349@163.com>
2022-07-23 22:14:01 +0800

    修改了部分运作流程；对物品进行了重命名

--- src/main/java/trioxwater/asymmetric/Asymmetric.java
+++ src/main/java/trioxwater/asymmetric/Asymmetric.java
@@ -2,14 +2,13 @@ package trioxwater.asymmetric;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Asymmetric implements ModInitializer {
 
-    public static final TrackTracerItem TRACK_TRACER = new TrackTracerItem(new FabricItemSettings().group(ItemGroup.REDSTONE).fireproof());
+    public static final RouteTrackerItem TRACK_TRACER = new RouteTrackerItem(new FabricItemSettings().group(ItemGroup.REDSTONE).fireproof());
 
     @Override
     public void onInitialize() {
--- src/main/java/trioxwater/asymmetric/RouteTrackerItem.java
+++ src/main/java/trioxwater/asymmetric/RouteTrackerItem.java
@@ -6,9 +6,7 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.particle.ParticleEffect;
-import net.minecraft.particle.ParticleType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
@@ -18,36 +16,49 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import java.util.List;
 
-public class TrackTracerItem extends Item {
-    public TrackTracerItem(Settings settings) {
+public class RouteTrackerItem extends Item {
+    public RouteTrackerItem(Settings settings) {
         super(settings);
     }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         if (world.isClient()) return super.use(world, playerEntity, hand);
-        PlayerEntity closestPlayer = world.getClosestPlayer(playerEntity, 50.0);
         List<? extends PlayerEntity> list;
         list = world.getPlayers();
-        Vec3d playerLoc = playerEntity.getPos();
+        PlayerEntity closestPlayer = null;
+        double closestDis = 20000.0;
+        Vec3d userPos = playerEntity.getPos();
+        //查找最近玩家
         for (PlayerEntity player: list) {
             Vec3d curPos = player.getPos();
-            Vec3d v = curPos.relativize(playerLoc); //获取一个指向玩家的向量
-            Vec3d fv = v.normalize();
-            //ParticleEffect ParticleEffect = new ParticleType<>();
-            //world.addParticle(ParticleEffect, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(),
-            //        fv.getX(), fv.getY(), fv.getZ());
+            if (player != playerEntity) {
+                double dis = curPos.distanceTo(userPos);
+                if (dis < closestDis) {
+                    closestPlayer = player;
+                    closestDis = dis;
+                }
+            }
         }
-        if(closestPlayer != null) {
-            //赋予该玩家发光效果，持续20秒
+        if (closestPlayer != null) {
+            if (closestDis <= 75.00) {
+                Vec3d v = closestPlayer.getPos().relativize(userPos).normalize();
+                for (int i = 0; i < 12; ++i)
+                    world.addParticle(ParticleTypes.PORTAL, playerEntity.getX(), playerEntity.getY(),
+                            playerEntity.getZ(), v.getX() * 2, v.getY() * 2, v.getZ() * 2);
+            }
+            //赋予该玩家发光效果，持续30秒
             closestPlayer.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 600, 1));
         }
+        playerEntity.getItemCooldownManager().set(this, 1200);
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
 
     @Override
     public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
-        tooltip.add(new TranslatableText("获取其他玩家的大致方位信息，并使50m内最近的玩家发光30秒。" +
-                "\n指示有一定偏差，距离越近，方位越准确。").formatted(Formatting.RED) );
+        tooltip.add(new TranslatableText
+                ("item.asymmetric.route_tracker.tooltip1"));
+        tooltip.add(new TranslatableText
+                ("item.asymmetric.route_tracker.tooltip2").formatted(Formatting.RED));
     }
 }
